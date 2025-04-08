1faab9c0ec37dc6a49d0d4bee4babba8a05f9060
abraham757 <abrahan.egonzalez@gmail.com>
2025-04-08 15:40:08 -0400

    fix vite.config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   server: {
     port: 3000,
     host: '0.0.0.0',
+    allowedHosts: ['deploy-full-stack-apps03.onrender.com'],
     open: true,
     proxy: {
       '/api': {
